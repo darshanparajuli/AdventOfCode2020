5cc50a2946aa8a66dc01f999bd374812dd5d1675
Darshan Parajuli <parajulidarshan@gmail.com>
2020-12-01 14:19:42 -0800

    Day 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,6 +1,42 @@
 use aoc_2020::read_input;
 
 fn main() {
-    let input = read_input();
-    dbg!(input);
+    let input: Vec<_> = read_input()
+        .iter()
+        .map(|e| e.parse::<u32>().unwrap())
+        .collect();
+    part1(&input);
+    part2(&input);
+}
+
+fn part1(input: &[u32]) {
+    let mut answer = 0;
+
+    'outer: for i in input {
+        for j in input.iter().filter(|e| *e != i) {
+            if i + j == 2020 {
+                answer = i * j;
+                break 'outer;
+            }
+        }
+    }
+
+    println!("part 1: {}", answer);
+}
+
+fn part2(input: &[u32]) {
+    let mut answer = 0;
+
+    'outer: for i in input {
+        for j in input.iter().filter(|e| *e != i) {
+            for k in input.iter().filter(|e| *e != j) {
+                if i + j + k == 2020 {
+                    answer = i * j * k;
+                    break 'outer;
+                }
+            }
+        }
+    }
+
+    println!("part 2: {}", answer);
 }
