d7f71668d3412ef20f790111767f22794241c602
Darshan Parajuli <parajulidarshan@gmail.com>
2020-12-06 12:42:44 -0800

    Update read_input

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,14 @@ use std::io::BufReader;
 
 pub fn read_input() -> Vec<String> {
     let arg = env::args().skip(1).next().unwrap();
-    BufReader::new(File::open(arg).unwrap())
-        .lines()
-        .map(|line| line.unwrap())
-        .collect()
+    match File::open(&arg) {
+        Ok(f) => BufReader::new(f)
+            .lines()
+            .map(|line| line.unwrap())
+            .collect(),
+        Err(e) => {
+            eprintln!("Error opening input file '{}': {}", arg, e);
+            std::process::exit(1);
+        }
+    }
 }
