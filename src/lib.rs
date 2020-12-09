1dc6be532a338af27d2aafefa3fe7d3604aa70d6
Darshan Parajuli <parajulidarshan@gmail.com>
2020-12-08 22:18:48 -0800

    Updated lib

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,15 @@ use std::io::prelude::*;
 use std::io::BufReader;
 
 pub fn read_input() -> Vec<String> {
+    read_input_map(|e| e)
+}
+
+pub fn read_input_map<T>(mapper: fn(String) -> T) -> Vec<T> {
     let arg = env::args().skip(1).next().unwrap();
     match File::open(&arg) {
         Ok(f) => BufReader::new(f)
             .lines()
-            .map(|line| line.unwrap())
+            .map(|line| mapper(line.unwrap()))
             .collect(),
         Err(e) => {
             eprintln!("Error opening input file '{}': {}", arg, e);
